afe62bfb38684c0c2a608941bc0ea56dbb8d0575
Вихарев Алексей <a.v.vikharev@gmail.com>
2022-09-25 18:19:26 +0300

    commit

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -9,17 +9,16 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('')
-        print(response.url)
+        response = self.guest_client.get('infra_app/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('infra_app/second/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        response = self.guest_client.get('')
+        response = self.guest_client.get('infra_app/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('infra_app/second/')
         self.assertContains(response, 'А это вторая страница!')
